06b200f691d993b67100c7bab9f8b8af37261b91
PanValeriy <64436706+PanValeriy@users.noreply.github.com>
2020-07-09 14:41:05 +0300

    Update Task9.py
    changed variable

--- Task9.py
+++ Task9.py
@@ -1,6 +1,6 @@
-nomerKvytka = input('Введіть номер квитка (6зн)= ')
-x = int(nomerKvytka[0]) + int(nomerKvytka[1]) + int(nomerKvytka[2])
-y = int(nomerKvytka[3]) + int(nomerKvytka[4]) + int(nomerKvytka[5])
+nomer_kvytka = input('Введіть номер квитка (6зн)= ')
+x = int(nomer_kvytka[0]) + int(nomer_kvytka[1]) + int(nomer_kvytka[2])
+y = int(nomer_kvytka[3]) + int(nomer_kvytka[4]) + int(nomer_kvytka[5])
 if x==y:
     print('Вітаю у Вас Щасливий квиток')
 else:
